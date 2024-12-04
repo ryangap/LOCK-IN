54bcf00ab61efc365dfe8a3ae5dd3ab344d93a13
ryangap <127233822+ryangap@users.noreply.github.com>
2024-12-04 08:53:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-let trilho = document.getElementById('trilho')
+let currentIndex = 0;
 
-trilho.addEventListener('click', ()=>{
-    trilho.classList.toggle('dark')
-})
+function moveCarousel(direction) {
+    const carousel = document.querySelector('.carousel');
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalItems - 1;
+    } else if (currentIndex >= totalItems) {
+        currentIndex = 0;
+    }
+
+    const offset = -currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+}
+
+
+
